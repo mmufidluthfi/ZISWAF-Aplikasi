8748691560425ae6881f7021e101e48c95cb0d3f
Muhammad Mufid Luthfi <mufiedkhamardos@gmail.com>
2016-04-14 23:44:58 +0700

    dashboard
    update dashboard

--- resources/views/dashboard/dashboard-home.blade.php
+++ resources/views/dashboard/dashboard-home.blade.php
@@ -2,7 +2,13 @@
 
 	@section('content')
 		<section class="content">
-			<center><h1>SELAMAT DATANG</h1></center>
+			<div class="widget-container">
+			<center>
+					<a href="{{ url('/dashboard/pendanaan')}}"><img src="{{URL::to('dashboard/images/button_pendanaan.png')}}" alt="" width="300" height="300" /></a> 
+					<a href="{{ url('/dashboard/laporan')}}"><img src="{{URL::to('dashboard/images/button_laporan.png')}}" alt="" width="300" height="300" /></a> 
+					<a href="{{ url('/dashboard/edit-profile')}}"><img src="{{URL::to('dashboard/images/button_profile.png')}}" alt="" width="300" height="300" /></a> 
+			</center>
+			</div>
 		</section>
 	
 @endsection
